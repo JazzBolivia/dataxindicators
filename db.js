1990914c94adc649bb280e1beca34049c1883d92
unknown <you@example.com>
2021-10-19 16:05:55 -0400

    Subiendo los primeros Archivos

--- db.js
+++ db.js
@@ -1,5 +1,16 @@
 const Pool = require("pg").Pool;
 
+/*
+const pool = new Pool({
+    user: "postgres",
+    password: "datax",
+    database: "SPIM_M",
+    host: "181.115.167.239",
+    port: 5432
+});
+
+*/
+/*
 const pool = new Pool({
     user: "postgres",
     password: "datax",
@@ -7,6 +18,22 @@ const pool = new Pool({
     host: "10.0.0.9",
     port: 5432
 });
+
+*/
+
+
+
+const pool = new Pool({
+    user: "postgres",
+    password: "datax",
+    database: "SPIM_M",
+    host: "127.0.0.1",
+    port: 5432
+});
+
+
+
+
 
 /*
 const pool = new Pool({
